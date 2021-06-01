a41b0f932d549bf2832e6a24d9c18a61de817145
Nimaoth <nicolas.otremba@web.de>
2021-06-01 10:43:18 +0200

    removed 3rd layout

--- neo/build_lily58_rev1_layout_mine.sh
+++ neo/build_lily58_rev1_layout_mine.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-qmk json2c Desktop/Keyboard/neo/lily58_rev1_layout_mine.json > ~/qmk_firmware/keyboards/lily58/keymaps/nimaoth/keymap.c
+qmk json2c Desktop/Keyboard/neo/lily58_rev1_layout_mine.json > ~/qmk_firmware/keyboards/lily58/keymaps/nimaoth/keymap.gen.c
 qmk compile -kb lily58 -km nimaoth
